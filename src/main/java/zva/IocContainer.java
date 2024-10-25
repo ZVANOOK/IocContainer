e4f53f5f9625a3ae18da850a5de9d79e5a6a8e99
ZakharovVA4 <ZakharovVA4@it.mos.ru>
2024-10-25 10:41:49 +0300

    Код + тесты

---
+++ src/main/java/zva/IocContainer.java
@@ -0,0 +1,21 @@
+package zva;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class IocContainer {
+    private final Map<String, Function<Object[], ?>> registry = new HashMap<>();
+
+    public <T> T resolve(String key, Object... args) {
+        Function<Object[], ?> function = registry.get(key);
+        if (function != null) {
+            return (T) function.apply(args);
+        }
+        throw new IllegalArgumentException("No registration for key: " + key);
+    }
+
+    public <T> void register(String key, Function<Object[], T> function) {
+        registry.put(key, function);
+    }
+}
---
+++ src/main/java/zva/RotateCommand.java
@@ -0,0 +1,8 @@
+package zva;
+
+public class RotateCommand implements Command {
+    @Override
+    public void execute() {
+        System.out.println("Поворачиваем");
+    }
+}
